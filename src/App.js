a332cd38d663968542c11f7d514eee16d26e911f
ajit3190 <shettyajit9@gmail.com>
2025-03-17 12:40:11 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* // <header className="App-header"> */}
+      {/* //   <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* //   <p> */}
+      {/* //     Edit <code>src/App.js</code> and save to reload. */}
+      {/* //   </p> */}
+      {/* //   <a */}
+      {/* //     className="App-link" */}
+      {/* //     href="https://reactjs.org" */}
+      {/* //     target="_blank" */}
+      {/* //     rel="noopener noreferrer" */}
+      {/* //   > */}
+      {/* //     Learn React */}
+      {/* //   </a> */}
+      {/* // </header> */}
+      {/* <button>I'm a button</button> */}
+    <h1>Welcome to my app</h1>
+    <MyButton />
+      {/* <p> fff</p> */}
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+
+    <button>
+      I'm a button
+    </button>
   );
 }
 
